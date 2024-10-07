4d6eec2d87197e117b670d189b1a8bcd4dcbb3a1
JosephChristenson <Jechristenson@gmail.com>
2024-10-06 21:59:44 -0500

    Some Problems Solved

--- Problems/1. Two Sum.go
+++ Problems/1. Two Sum.go
@@ -1,45 +1,31 @@
 package main
 
-import "fmt"
-
-func main() {
-	nums := []int{2, 7, 11, 15}
-	target := 9
-	fmt.Print(twoSums(nums, target))
-}
-
-// Brute Force answer
-func twoSums(nums []int, target int) []int {
-
-	for firstindex, element := range nums {
-		for secondindex, secondelement := range nums {
-			if firstindex != secondindex {
-				if (element + secondelement) == target {
-					result := []int{firstindex, secondindex}
-					return result
-				}
-			}
-		}
-	}
-
-	result := []int{0}
-	return result
-}
-
-// package main
-
-// type IntHeap []int
-
-// func (h IntHeap) Len() int           { return len(h) }
-// func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-// func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
-// func (h *IntHeap) Pop() any {
-//     old := *h
-//     n := len(old)
-//     x := old[n-1]
-//     *h = old[0 : n-1]
-//     return x
+// func main() {
+// 	nums := []int{2, 7, 11, 15}
+// 	target := 9
+// 	fmt.Print(twoSums(nums, target))
+// 	nums = []int{3, 2, 4}
+// 	target = 6
+// 	fmt.Print(twoSums(nums, target))
+// 	nums = []int{3, 3}
+// 	target = 6
+// 	fmt.Print(twoSums(nums, target))
 // }
-// func (h *IntHeap) Push(x any) {
-//     *h = append(*h, x.(int))
+
+//Brute Force answer
+// func twoSums(nums []int, target int) []int {
+
+// 	for firstindex, element := range nums {
+// 		for secondindex, secondelement := range nums {
+// 			if firstindex != secondindex {
+// 				if (element + secondelement) == target {
+// 					result := []int{firstindex, secondindex}
+// 					return result
+// 				}
+// 			}
+// 		}
+// 	}
+
+// 	result := []int{0}
+// 	return result
 // }
---
+++ Problems/88. Merge Sorted Array.go
@@ -0,0 +1,19 @@
+package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	nums1 := []int{1, 2, 3, 0, 0, 0}
+// 	nums2 := []int{2, 5, 6}
+// 	m := 3
+// 	n := 3
+// 	merge(nums1, m, nums2, n)
+// 	fmt.Print(nums1)
+// }
+
+// func merge(nums1 []int, m int, nums2 []int, n int) {
+// 	i, j := m, n
+
+// }
